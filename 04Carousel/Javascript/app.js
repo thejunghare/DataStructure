7aab60fc7f3b53e78c3e808ef04b4f960e16b1d9
sjprasad-bit <prasadjunghare2@gmail.com>
2022-11-29 22:50:35 +0530

    New folder added

--- 04Carousel/Javascript/app.js
+++ 04Carousel/Javascript/app.js
@@ -1,16 +1,31 @@
 // Catch the slide show img
-const imageOne = document.getElementById('img-1')
+const image = document.getElementById('img-1')
 const imageTwo = document.getElementById('img-2')
+
+const slideShow = document.getElementsByClassName('slide-show')
 
 // Catch the next button
 const rightArrow = document.getElementById('right-arrow')
 const leftArrow = document.getElementById('left-arrow')
 
-// add event listener to unhide img when clicked on right arrow
-rightArrow.addEventListener('click', function () {
-    imageTwo.style.display = 'block'
-    console.log('I was Clicked')
-})
+//making an array object
+const arr = [{
+    id: 1,
+    img: 'https://github.com/sjprasad-bit/JavaScript-Projects/blob/main/04Carousel/img/img1.jpg'
+}, {
+    id: 2,
+    img: 'https://github.com/sjprasad-bit/JavaScript-Projects/blob/main/04Carousel/img/img2.jpg'
+}]
 
-// Test the catch
-console.log(rightArrow)
+//setting inital slide to 0 (array's index starts at zero)
+let currentSlide = 1
+
+// load inital item
+window.addEventListener('DOMContentLoaded', function () {
+    let item = arr[currentSlide]
+    image.src = item.image
+
+//    slideShow.style.display = 'block'
+
+    console.log('loaded')
+})
