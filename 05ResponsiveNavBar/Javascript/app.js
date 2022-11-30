992b77d98d1ac18a7c11dcba5174ddd6a0dcb9a0
sjprasad-bit <prasadjunghare2@gmail.com>
2022-11-30 22:58:36 +0530

    30LTSCommit

--- 05ResponsiveNavBar/Javascript/app.js
+++ 05ResponsiveNavBar/Javascript/app.js
@@ -1,8 +1,14 @@
-// hide sidebar once clicked outside div
+const open_sidebar = document.getElementById('icon')
 const sidebar = document.getElementById('sidebar')
 
-document.addEventListener('click', function(event){
-    if (!sidebar.contains(event.target)){
+// show sidebar
+open_sidebar.addEventListener('click', function () {
+    sidebar.style.display = 'block'
+})
+
+// hide sidebar once clicked outside div
+document.addEventListener('click', function (event) {
+    if (!sidebar.contains(event.target)) {
         sidebar.style.display = 'none'
     }
 })
