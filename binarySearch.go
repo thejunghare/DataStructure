5bccfc30e4baddb094f1384c0be797db31e10775
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-08-04 15:04:02 +0530

    added binary search implementation

--- binarySearch.go
+++ binarySearch.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
-func binarySearch(nums []int, x int) int {
-	for i:=0; i<len(nums);i++{
-		
+func binarySearch(nums []int, target int) int {
+	start := 0
+	end := len(nums) - 1
+
+	for start <= end {
+		mid := (start + end) / 2
+		if nums[mid] < target {
+			start = mid + 1
+		} else if nums[mid] > target {
+			end = mid - 1
+		} else {
+			return mid
+		}
 	}
+
 	return -1
 }
 
 func main() {
 	nums := []int{10, 12, 24, 29, 39, 40, 51, 56, 69}
-	x := 56
-	fmt.Println(binarySearch(nums, x))
+	target := 10
+	fmt.Print(binarySearch(nums, target))
 }
